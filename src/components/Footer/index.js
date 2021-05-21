2681a87ae2d264b488b034521cd3500eb9b12786
Lucas Frigo de Souza <lfrigodesouza@outlook.com>
2021-05-21 19:31:10 -0300

    Corrige responsividade

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -19,11 +19,15 @@ const FooterBar = styled.footer`
 
 const BeerButton = styled.span`
   cursor: pointer;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
 `;
 
 const BUY_ME_A_BEER_DESCRIPTION = `Gostou dos meus conteúdos? Porque não aproveita e me compra uma cerveja? Utilize o QRCode abaixo (ou o código da chave) para fazer um Pix de qualquer valor! <br> Vai me ajudar tanto com cerveja, quanto com as despesas dos conteúdos que eu produzo!
 
-<span class="drac-text-black-secondary drac-mb-sm drac-mt-sm">5c80c88b-d1e7-4799-a185-dd8517d1671e</span>
+<span class="drac-text-black-secondary drac-mb-sm drac-mt-sm drac-text-center">5c80c88b-d1e7-4799-a185-dd8517d1671e</span>
 <img width="70%" src="/qrcode-pix.png"/>`;
 
 export default function Footer({ fixed }) {
@@ -38,12 +42,15 @@ export default function Footer({ fixed }) {
   }
   return (
     <FooterBar fixed={fixed}>
-      <Text as="p" color="purpleCyan">
-        LFrigoDeSouza.NET | © 2020 – 2021 |
+      <Text as="p" color="purpleCyan" size="sm">
+        LFrigoDeSouza.NET | © 2020 – 2021
         {' '}
-        <BeerButton onClick={beerClickHandler}>Buy me a beer</BeerButton>
+        <BeerButton onClick={beerClickHandler}>
+          Buy me a beer
+          <Text as={TiBeer} color="cyan" />
+        </BeerButton>
       </Text>
-      <Text as={TiBeer} color="cyan" />
+
       <ModalMessage
         modalCloseHandler={modalCloseHandler}
         showModal={showModal}
