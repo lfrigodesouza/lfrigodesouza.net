4aab17f909f27ab8ab393768fae5a0ad5b3ea389
Lucas Frigo de Souza <lucas@lfrigodesouza.net>
2023-04-23 11:58:53 -0300

    Atualiza seção sobre

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -16,21 +16,22 @@ export default function About() {
         Olá, boas vindas!
       </Title>
       <Paragraph align="justify" lineHeight="lg">
-        Me chamo Lucas, sou formado em Engenharia da Computação, tenho MBA em Engenharia de Software
-        e estou cursando um MBA em CyberSecurity.
+        Me chamo Lucas, sou formado em Engenharia da Computação com MBAs em Engenharia de Software e
+        Cibersegurança.
         <br />
-        Com mais de 10 anos de experiência em TI, já trabalhei como analista de suporte, analista de
-        teste/QA, desenvolvedor, e atualmente atuo como Líder Técnico.
+        Com mais de 10 anos de experiência em TI, já trabalhei como analista de suporte,
+        analista de teste/QA, desenvolvedor e atualmente atuo como líder técnico.
         <br />
         <br />
-        Meu forte é programação backend com .NET e C#, porém já trabalhei com diversas tecnologias,
-        incluindo tecnologias de frontend, como Angular e React, então me considero como fullstack.
-        Também tenho experiência com SQLServer, Oracle, Jenkins, Azure, AWS e outras tecnologias.
+        Meu forte é programação backend com .NET e C#, porém já trabalhei com diversas
+        outras tecnologias, como Angular, React e Python. Também tenho experiência com
+        SQLServer, Oracle, MongoDB, Redis, Jenkins, Azure, AWS, Docker, Kubernetes, entre outras.
         <br />
         Também tenho um foco em segurança da informação e desenvolvimento seguro (DevSec).
         <br />
-        Trabalhei por alguns anos na área da Indústria 4.0, e hoje trabalho no mercado financeiro,
-        na área de investimentos de uma grande fintech brasileira.
+        Trabalhei por alguns anos na área da Indústria 4.0, e já atuei no mercado de
+        adquirência e no mercado financeiro (na área de investimentos de uma grande fintech
+        brasileira). Atualmente estou atuando no mercado imobiliário para uma empresa dos EUA.
         <br />
       </Paragraph>
       <SocialMedias />
